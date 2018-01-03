09d58db026e1acb5911c2787206ffbb84a6f88ab
Jim Lin <jimlintw922@gmail.com>
2018-01-03 22:41:59 +0800

    add 2-ovs-br-vxlan-vlan

---
+++ vagrant/scripts/vms/2-ovs-br-vxlan-vlan/vm1-2-ovs-br-vxlan-vlan.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+ovs-vsctl add-br br0
+echo "$(date) | created ovs bridge br0"
+
+ip addr flush dev enp0s8
+echo "$(date) | cleared dev enp0s8"
+
+ovs-vsctl add-port br0 enp0s8
+echo "$(date) | attached dev enp0s8 to ovs br br0"
+
+ip addr add 192.168.33.102/24 dev br0
+echo "$(date) | assigned ip 192.168.33.102 to ovs br br0"
+
+ip link set dev br0 up
+echo "$(date) | brought up ovs br br0"
+
+#echo "ping vm1"
+#ping 192.168.33.101
+
+ovs-vsctl add-br br1
+echo "$(date) | created ovs bridge br1"
+
+# TODO
+# (1) tap
+# (2) attach and assign vlan tag
+# (3) assign ip
+#ip addr add 10.10.10.102/24 dev br1
+#echo "$(date) | assigned ip 10.10.10.102 to ovs br br1"
+
+ip link set dev br1 up
+echo "$(date) | brought up ovs br br1"
+
+ovs-vsctl add-port br1 vx1 -- set interface vx1 type=vxlan options:remote_ip=192.168.33.101
+echo "$(date) | created vxlan port vx1 to ovs br br1"
+
+#echo "ping vm1 via vxlan"
+#ping 10.10.10.101
---
+++ vagrant/scripts/vms/2-ovs-br-vxlan-vlan/vm2-2-ovs-br-vxlan-vlan.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+ovs-vsctl add-br br0
+echo "$(date) | created ovs bridge br0"
+
+ip addr flush dev enp0s8
+echo "$(date) | cleared dev enp0s8"
+
+ovs-vsctl add-port br0 enp0s8
+echo "$(date) | attached dev enp0s8 to ovs br br0"
+
+ip addr add 192.168.33.101/24 dev br0
+echo "$(date) | assigned ip 192.168.33.101 to ovs br br0"
+
+ip link set dev br0 up
+echo "$(date) | brought up ovs br br0"
+
+#echo "ping vm2"
+#ping 192.168.33.102
+
+ovs-vsctl add-br br1
+echo "$(date) | created ovs bridge br1"
+
+# TODO
+# (1) tap
+# (2) attach and assign vlan tag
+# (3) assign ip
+#ip addr add 10.10.10.101/24 dev br1
+#echo "$(date) | assigned ip 10.10.10.101 to ovs br br1"
+
+ip link set dev br1 up
+echo "$(date) | brought up ovs br br1"
+
+ovs-vsctl add-port br1 vx1 -- set interface vx1 type=vxlan options:remote_ip=192.168.33.102
+echo "$(date) | created vxlan port vx1 to ovs br br1"
+
+#echo "ping vm2 via vxlan"
+#ping 10.10.10.102
