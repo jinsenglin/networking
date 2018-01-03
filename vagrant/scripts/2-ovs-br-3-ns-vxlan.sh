fd74d87179ff1db7d1e811b2b142afb642f15403
Jim Lin <jimlintw922@gmail.com>
2018-01-03 16:59:45 +0800

    update README

--- vagrant/scripts/2-ovs-br-3-ns-vxlan.sh
+++ vagrant/scripts/2-ovs-br-3-ns-vxlan.sh
@@ -23,32 +23,38 @@ echo "$(date) | assigned 10.10.10.1/24 to ns1::tap0"
 ip netns exec ns2 ip addr add 10.10.10.2/24 dev tap1
 echo "$(date) | assigned 10.10.10.2/24 to ns2::tap1"
 
+ip netns exec ns1 ip link set tap0 up
+echo "$(date) | brought up ns1::tap0"
+
+ip netns exec ns2 ip link set tap1 up
+echo "$(date) | brought up ns2::tap1"
+
 ip netns exec ns1 ping -c 1 10.10.10.2
 echo "$(date) | from ns1 ping ns2"
 
 ip netns exec ns2 ping -c 1 10.10.10.1
 echo "$(date) | from ns2 ping ns1"
 
-ip netns exec ns1 ovs-vsctl add-br br0
-echo "$(date) | created ovs bridge ns1::br0"
-
-ip netns exec ns2 ovs-vsctl add-br br0
-echo "$(date) | created ovs bridge ns2::br0"
-
-ip netns exec ns1 ip addr add 10.10.20.1/24 dev br0
-echo "$(date) | assigned 10.10.20.1/24 to ns1::br0"
-
-ip netns exec ns2 ip addr add 10.10.20.2/24 dev br0
-echo "$(date) | assigned 10.10.20.2/24 to ns2::br0"
-
-ip netns exec ns1 ovs-vsctl add-port br0 vx1 -- set interface vx1 type=vxlan options:remote_ip=10.10.10.2
-echo "$(date) | created vxlan port on ns1::br0"
-
-ip netns exec ns2 ovs-vsctl add-port br0 vx1 -- set interface vx1 type=vxlan options:remote_ip=10.10.10.1
-echo "$(date) | created vxlan port on ns2::br0"
-
-ip netns exec ns1 ping -c 1 10.10.20.2
-echo "$(date) | from ns1 ping ns2 via vxlan"
-
-ip netns exec ns2 ping -c 1 10.10.20.1
-echo "$(date) | from ns2 ping ns1 via vxlan"
+#ip netns exec ns1 ovs-vsctl add-br br0
+#echo "$(date) | created ovs bridge ns1::br0"
+#
+#ip netns exec ns2 ovs-vsctl add-br br0
+#echo "$(date) | created ovs bridge ns2::br0"
+#
+#ip netns exec ns1 ip addr add 10.10.20.1/24 dev br0
+#echo "$(date) | assigned 10.10.20.1/24 to ns1::br0"
+#
+#ip netns exec ns2 ip addr add 10.10.20.2/24 dev br0
+#echo "$(date) | assigned 10.10.20.2/24 to ns2::br0"
+#
+#ip netns exec ns1 ovs-vsctl add-port br0 vx1 -- set interface vx1 type=vxlan options:remote_ip=10.10.10.2
+#echo "$(date) | created vxlan port on ns1::br0"
+#
+#ip netns exec ns2 ovs-vsctl add-port br0 vx1 -- set interface vx1 type=vxlan options:remote_ip=10.10.10.1
+#echo "$(date) | created vxlan port on ns2::br0"
+#
+#ip netns exec ns1 ping -c 1 10.10.20.2
+#echo "$(date) | from ns1 ping ns2 via vxlan"
+#
+#ip netns exec ns2 ping -c 1 10.10.20.1
+#echo "$(date) | from ns2 ping ns1 via vxlan"
--- vagrant/scripts/README.md
+++ vagrant/scripts/README.md
@@ -5,3 +5,9 @@ dhclient tap0.100 PASSED
 dhclient tap2.100 FAILED
 ip netns exec ns2 dhclient tap3.100 FAILED
 ```
+
+2-ovs-br-3-ns-vxlan.sh FAILED
+
+```
+ovs br only exists in root ns. i.e. ovs br can not be created in other ns.
+```
